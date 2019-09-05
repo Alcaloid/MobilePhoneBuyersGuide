dc29ec8b4ea80c62653d8031dd386ccc24b23c11
Alcaloid <apichai.1507@mail.kmutt.ac.th>
2019-09-05 09:12:32 +0700

    Commit for create brach unit tester

---
+++ app/src/test/java/com/codemobile/mobilephonebuyersguide/FavoritePageUnitTest.kt
@@ -0,0 +1,13 @@
+package com.codemobile.mobilephonebuyersguide
+
+import com.codemobile.mobilephonebuyersguide.model.MobileListResponse
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+class FavoritePageUnitTest {
+//    @Test
+//    fun testSortFavoriteArray() {
+//        val arrayList:ArrayList<MobileListResponse> =
+//    }
+}
